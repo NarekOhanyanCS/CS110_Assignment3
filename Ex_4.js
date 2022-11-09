65d7f5e3feb6b9210178542593eb158dce6ddf11
Narek Ohanyan <117839726+NarekOhanyanCS@users.noreply.github.com>
2022-11-09 20:29:36 +0400

    Delete Ex_4.js

--- Ex_4.js
+++
@@ -1,8 +0,0 @@
-function reverseArray (arr) {
-    var newArr = [];
-    for (var i = 0, j = arr.length - 1; i < arr.length; i++, j--) {      
-        newArr[i] = arr[j];
-    }   
-    return newArr;
-}
-console.log(reverseArray([1, 5, 4, 18, 3, 0]));
